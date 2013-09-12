1a90d27fc68d392cafcc4266cae17f361e46d326
Tom Maiaroto <tom@shift8creative.com>
2013-09-12 00:03:10 -0500

    updating installer

--- install.sh
+++ install.sh
@@ -2,4 +2,5 @@
 
 echo "Getting Blackprint from GitHub"
 echo ""
-exec git clone git://github.com/tmaiaroto/blackprint.git .; exec setup.sh
+exec (git clone git://github.com/tmaiaroto/blackprint.git . 2>&1);
+exec setup.sh
