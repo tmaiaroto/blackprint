406932787bbccf1493d64dab68569014f2f66c73
Tom Maiaroto <tom@shift8creative.com>
2013-11-08 20:32:35 -0600

    updating helper for datepicker field

--- libraries/blackprint/extensions/helper/BlackprintForm.php
+++ libraries/blackprint/extensions/helper/BlackprintForm.php
@@ -149,13 +149,10 @@ class BlackprintForm extends \lithium\template\helper\Form {
 			// Other options include; data-date-format, data-date, data-date-viewmode, data-date-minviewmode, etc.
 			// @see http://www.eyecon.ro/bootstrap-datepicker/ for more...
 			// But catch a special option, 'append'
-			$append = '';
 			$appendClass = '';
-			if(isset($options['append'])) {
+			if(!empty($append)) {
 				$appendClass = !empty($inputDivClass) ? ' input-append':'input-append';
-				// If append is a string, then specific HTML was being passed to append, otherwise use the default calendar icon
 				$append = (is_string($options['append'])) ? $options['append']:'<span class="add-on"><i class="fa fa-calendar"></i></span>';
-				unset($options['append']); // no sense passing it along, it could even break things given it would be HTML code within an element attribute value
 			}
 
 			$options['template'] = '<div class="' . $groupClass . '" style="' . $groupStyle . '"{:wrap}>{:label}<div class="' . $inputDivClass .  $appendClass . '">' . $prepend . '{:input}' . $append . '</div>';
