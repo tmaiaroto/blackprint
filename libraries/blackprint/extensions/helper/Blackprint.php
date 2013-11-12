44c17f0db8c989a06b3d51859def33493e791d48
Tom Maiaroto <tom@shift8creative.com>
2013-11-12 13:39:13 -0600

    helper summarize method

--- libraries/blackprint/extensions/helper/Blackprint.php
+++ libraries/blackprint/extensions/helper/Blackprint.php
@@ -356,21 +356,19 @@ class Blackprint extends \lithium\template\helper\Html {
 		$i = 0;
 		$tags = array();
 
-		if($isHTML) {
-			preg_match_all('/<[^>]+>([^<]*)/', $s, $m, PREG_OFFSET_CAPTURE | PREG_SET_ORDER);
-			foreach($m as $o) {
-				if($o[0][1] - $i >= $l) {
-					break;                  
-				}
-				$t = substr(strtok($o[0][0], " \t\n\r\0\x0B>"), 1);
-				if($t[0] != '/') {
-					$tags[] = $t;                   
-				}
-				elseif(end($tags) == substr($t, 1)) {
-					array_pop($tags);                   
-				}
-				$i += $o[1][1] - $o[0][1];
+		preg_match_all('/<[^>]+>([^<]*)/', $s, $m, PREG_OFFSET_CAPTURE | PREG_SET_ORDER);
+		foreach($m as $o) {
+			if($o[0][1] - $i >= $l) {
+				break;                  
 			}
+			$t = substr(strtok($o[0][0], " \t\n\r\0\x0B>"), 1);
+			if($t[0] != '/') {
+				$tags[] = $t;                   
+			}
+			elseif(end($tags) == substr($t, 1)) {
+				array_pop($tags);                   
+			}
+			$i += $o[1][1] - $o[0][1];
 		}
 		$output = substr($s, 0, $l = min(strlen($s), $l + $i)) . (count($tags = array_reverse($tags)) ? '</' . implode('></', $tags) . '>' : '') . $e;
 		return $output;
