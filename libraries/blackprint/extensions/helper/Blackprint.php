247b7a2f9069ea98c60ff7f6f97fa6ac86b5905f
Tom Maiaroto <tom@shift8creative.com>
2013-11-12 13:37:10 -0600

    helper summarize method

--- libraries/blackprint/extensions/helper/Blackprint.php
+++ libraries/blackprint/extensions/helper/Blackprint.php
@@ -309,18 +309,19 @@ class Blackprint extends \lithium\template\helper\Html {
 	*/
 	public function summarize($content=null, $options=array()) {
 		$options += array(
-			'limit' => 250,
+			'wordLimit' => 100,
 			'trail' => '...',
 			'alwaysTrail' => false,
 			'neverTrail' => false
 		);
-		if(empty($content) || !is_int($options['limit'])) {
+		if(empty($content) || !is_int($options['wordLimit'])) {
 			return '';
 		}
+		$content = strip_tags($content);
 
 		$trailNeeded = $options['alwaysTrail'];
-		$words = explode(' ', $content, ($options['limit'] + 1));
-		if(count($words) > $options['limit']) {
+		$words = explode(' ', $content, ($options['wordLimit'] + 1));
+		if(count($words) > $options['wordLimit']) {
 			$trailNeeded = true;
 			array_pop($words);
 		}
@@ -328,6 +329,50 @@ class Blackprint extends \lithium\template\helper\Html {
 			$trailNeeded = false;
 		}
 		return $trailNeeded ? implode(' ', $words) . $options['trail']:implode(' ', $words);
+	}
+
+	/**
+	 * Similar to the above method, but will take into consideration HTML tags.
+	 * However, this is not a word limit, it is a character limit.
+	 *
+	*/
+	public function summarizeHtml($content, $options=array()) {
+		$options += array(
+			'characterLimit' => 500,
+			'trail' => '&hellip';
+			'alwaysTrail' => false,
+			'neverTrail' => false
+		);
+
+		$s = trim($content);
+		$e = (strlen(strip_tags($s)) > $l) ? $options['trail'] : '';
+		if($options['alwaysTrail']) {
+			$e = $options['trail'];
+		}
+		if($options['neverTrail']) {
+			$e = '';
+		}
+		$i = 0;
+		$tags = array();
+
+		if($isHTML) {
+			preg_match_all('/<[^>]+>([^<]*)/', $s, $m, PREG_OFFSET_CAPTURE | PREG_SET_ORDER);
+			foreach($m as $o) {
+				if($o[0][1] - $i >= $l) {
+					break;                  
+				}
+				$t = substr(strtok($o[0][0], " \t\n\r\0\x0B>"), 1);
+				if($t[0] != '/') {
+					$tags[] = $t;                   
+				}
+				elseif(end($tags) == substr($t, 1)) {
+					array_pop($tags);                   
+				}
+				$i += $o[1][1] - $o[0][1];
+			}
+		}
+		$output = substr($s, 0, $l = min(strlen($s), $l + $i)) . (count($tags = array_reverse($tags)) ? '</' . implode('></', $tags) . '>' : '') . $e;
+		return $output;
 	}
 
 	/**
