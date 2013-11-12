4035e39f92d14854f9597552c1710f24c8633b3a
Tom Maiaroto <tom@shift8creative.com>
2013-11-12 13:43:17 -0600

    helper summarize method

--- libraries/blackprint/extensions/helper/Blackprint.php
+++ libraries/blackprint/extensions/helper/Blackprint.php
@@ -302,76 +302,104 @@ class Blackprint extends \lithium\template\helper\Html {
 	}
 
 	/**
-	 * Will limit the number of words in a string.
+	 * truncateHtml can truncate a string up to a number of characters while preserving whole words and HTML tags
 	 *
-	 * @param string $content
-	 * @param array $options
-	*/
-	public function summarize($content=null, $options=array()) {
-		$options += array(
-			'wordLimit' => 100,
-			'trail' => '...',
-			'alwaysTrail' => false,
-			'neverTrail' => false
-		);
-		if(empty($content) || !is_int($options['wordLimit'])) {
-			return '';
-		}
-		$content = strip_tags($content);
-
-		$trailNeeded = $options['alwaysTrail'];
-		$words = explode(' ', $content, ($options['wordLimit'] + 1));
-		if(count($words) > $options['wordLimit']) {
-			$trailNeeded = true;
-			array_pop($words);
-		}
-		if($options['neverTrail']) {
-			$trailNeeded = false;
-		}
-		return $trailNeeded ? implode(' ', $words) . $options['trail']:implode(' ', $words);
-	}
-
-	/**
-	 * Similar to the above method, but will take into consideration HTML tags.
-	 * However, this is not a word limit, it is a character limit.
+	 * @param string $text String to truncate.
+	 * @param integer $length Length of returned string, including ellipsis.
+	 * @param string $ending Ending to be appended to the trimmed string.
+	 * @param boolean $exact If false, $text will not be cut mid-word
+	 * @param boolean $considerHtml If true, HTML tags would be handled correctly
 	 *
-	*/
-	public function summarizeHtml($content, $options=array()) {
-		$options += array(
-			'characterLimit' => 500,
-			'trail' => '...',
-			'alwaysTrail' => false,
-			'neverTrail' => false
-		);
-
-		$s = trim($content);
-		$l = is_int($options['characterLimit']) ? $options['characterLimit']:500;
-		$e = (strlen(strip_tags($s)) > $l) ? $options['trail'] : '';
-		if($options['alwaysTrail']) {
-			$e = $options['trail'];
-		}
-		if($options['neverTrail']) {
-			$e = '';
-		}
-		$i = 0;
-		$tags = array();
-
-		preg_match_all('/<[^>]+>([^<]*)/', $s, $m, PREG_OFFSET_CAPTURE | PREG_SET_ORDER);
-		foreach($m as $o) {
-			if($o[0][1] - $i >= $l) {
-				break;                  
+	 * @return string Trimmed string.
+	 */
+	function truncateHtml($text, $length = 100, $ending = '...', $exact = false, $considerHtml = true) {
+		if ($considerHtml) {
+			// if the plain text is shorter than the maximum length, return the whole text
+			if (strlen(preg_replace('/<.*?>/', '', $text)) <= $length) {
+				return $text;
 			}
-			$t = substr(strtok($o[0][0], " \t\n\r\0\x0B>"), 1);
-			if($t[0] != '/') {
-				$tags[] = $t;                   
+			// splits all html-tags to scanable lines
+			preg_match_all('/(<.+?>)?([^<>]*)/s', $text, $lines, PREG_SET_ORDER);
+			$total_length = strlen($ending);
+			$open_tags = array();
+			$truncate = '';
+			foreach ($lines as $line_matchings) {
+				// if there is any html-tag in this line, handle it and add it (uncounted) to the output
+				if (!empty($line_matchings[1])) {
+					// if it's an "empty element" with or without xhtml-conform closing slash
+					if (preg_match('/^<(\s*.+?\/\s*|\s*(img|br|input|hr|area|base|basefont|col|frame|isindex|link|meta|param)(\s.+?)?)>$/is', $line_matchings[1])) {
+						// do nothing
+					// if tag is a closing tag
+					} else if (preg_match('/^<\s*\/([^\s]+?)\s*>$/s', $line_matchings[1], $tag_matchings)) {
+						// delete tag from $open_tags list
+						$pos = array_search($tag_matchings[1], $open_tags);
+						if ($pos !== false) {
+						unset($open_tags[$pos]);
+						}
+					// if tag is an opening tag
+					} else if (preg_match('/^<\s*([^\s>!]+).*?>$/s', $line_matchings[1], $tag_matchings)) {
+						// add tag to the beginning of $open_tags list
+						array_unshift($open_tags, strtolower($tag_matchings[1]));
+					}
+					// add html-tag to $truncate'd text
+					$truncate .= $line_matchings[1];
+				}
+				// calculate the length of the plain text part of the line; handle entities as one character
+				$content_length = strlen(preg_replace('/&[0-9a-z]{2,8};|&#[0-9]{1,7};|[0-9a-f]{1,6};/i', ' ', $line_matchings[2]));
+				if ($total_length+$content_length> $length) {
+					// the number of characters which are left
+					$left = $length - $total_length;
+					$entities_length = 0;
+					// search for html entities
+					if (preg_match_all('/&[0-9a-z]{2,8};|&#[0-9]{1,7};|[0-9a-f]{1,6};/i', $line_matchings[2], $entities, PREG_OFFSET_CAPTURE)) {
+						// calculate the real length of all entities in the legal range
+						foreach ($entities[0] as $entity) {
+							if ($entity[1]+1-$entities_length <= $left) {
+								$left--;
+								$entities_length += strlen($entity[0]);
+							} else {
+								// no more characters left
+								break;
+							}
+						}
+					}
+					$truncate .= substr($line_matchings[2], 0, $left+$entities_length);
+					// maximum lenght is reached, so get off the loop
+					break;
+				} else {
+					$truncate .= $line_matchings[2];
+					$total_length += $content_length;
+				}
+				// if the maximum length is reached, get off the loop
+				if($total_length>= $length) {
+					break;
+				}
 			}
-			elseif(end($tags) == substr($t, 1)) {
-				array_pop($tags);                   
+		} else {
+			if (strlen($text) <= $length) {
+				return $text;
+			} else {
+				$truncate = substr($text, 0, $length - strlen($ending));
 			}
-			$i += $o[1][1] - $o[0][1];
 		}
-		$output = substr($s, 0, $l = min(strlen($s), $l + $i)) . (count($tags = array_reverse($tags)) ? '</' . implode('></', $tags) . '>' : '') . $e;
-		return $output;
+		// if the words shouldn't be cut in the middle...
+		if (!$exact) {
+			// ...search the last occurance of a space...
+			$spacepos = strrpos($truncate, ' ');
+			if (isset($spacepos)) {
+				// ...and cut the text in this position
+				$truncate = substr($truncate, 0, $spacepos);
+			}
+		}
+		// add the defined ending to the text
+		$truncate .= $ending;
+		if($considerHtml) {
+			// close all unclosed html-tags
+			foreach ($open_tags as $tag) {
+				$truncate .= '</' . $tag . '>';
+			}
+		}
+		return $truncate;
 	}
 
 	/**
